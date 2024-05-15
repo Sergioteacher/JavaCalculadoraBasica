642f4b23ec21a5dcae77d89371208c8e4bf0ca5e
Sergio Teacher <Sergio Teacher@MiniWin>
2024-05-15 23:55:17 +0200

    Look

--- src/calculadoraBasica/CalculadoraBasica.java
+++ src/calculadoraBasica/CalculadoraBasica.java
@@ -13,6 +13,17 @@ import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.Font;
 import java.awt.Color;
+import javax.swing.JPanel;
+import java.awt.GridLayout;
+import java.awt.FlowLayout;
+import javax.swing.JTextField;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import java.awt.Component;
+import javax.swing.ImageIcon;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 
 
 /**
@@ -25,7 +36,28 @@ public class CalculadoraBasica {
 
 	private JFrame frmCalcuBasic;
 	private JLabel lblSize;
-	
+	private JPanel panel_Central;
+	private JPanel panel_nivel1;
+	private JPanel panel_nivel2;
+	private JPanel panel_nivel3;
+	private JPanel panel_nivel4;
+	private JPanel panel_nivel5;
+	private JTextField textField;
+	private JButton btnNewButton_2;
+	private JButton btnNewButton_1;
+	private JButton btnNewButton_3;
+	private JButton btnNewButton_4;
+	private JButton btnNewButton_5;
+	private JButton btnNewButton_6;
+	private JButton btnNewButton_7;
+	private JButton btnNewButton_8;
+	private JButton btnNewButton_9;
+	private JButton btnNewButton_0;
+	private JButton btnNewButton_Coma;
+	private JPanel panel_Izq;
+	private JButton btnNewButton_AC;
+	private JButton btnNewButton_Res;
+	private JLabel lblNewLabel;
 
 	/**
 	 * Este metodo estático se encarga de iniciar la ejecucion del programar
@@ -57,7 +89,7 @@ public class CalculadoraBasica {
 	 */
 	private void initialize() {
 
-		Dimension ini = new Dimension(209,209);
+		Dimension ini = new Dimension(224,209);
 		frmCalcuBasic = new JFrame();
 		frmCalcuBasic.setTitle("Calcu Basic");
 		frmCalcuBasic.addComponentListener(new ComponentAdapter() {
@@ -67,7 +99,7 @@ public class CalculadoraBasica {
 			}
 		});
 		
-		frmCalcuBasic.setBounds(100, 100, 224, 240);
+		frmCalcuBasic.setBounds(100, 100, 400, 350);
 		frmCalcuBasic.setMinimumSize(ini);
 		frmCalcuBasic.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmCalcuBasic.getContentPane().setLayout(new BorderLayout(0, 0));
@@ -79,9 +111,92 @@ public class CalculadoraBasica {
 		frmCalcuBasic.getContentPane().add(lblCabecera, BorderLayout.NORTH);
 		
 		lblSize = new JLabel(" ");
+		lblSize.setFont(new Font("Tahoma", Font.PLAIN, 9));
 		lblSize.setHorizontalAlignment(SwingConstants.CENTER);
 		frmCalcuBasic.getContentPane().add(lblSize, BorderLayout.SOUTH);
 		lblSize.setText(" ");
+		
+		panel_Central = new JPanel();
+		frmCalcuBasic.getContentPane().add(panel_Central, BorderLayout.CENTER);
+		panel_Central.setLayout(new GridLayout(5, 1, 0, 0));
+		
+		panel_nivel1 = new JPanel();
+		panel_Central.add(panel_nivel1);
+		FlowLayout fl_panel_nivel1 = new FlowLayout(FlowLayout.CENTER, 5, 5);
+		panel_nivel1.setLayout(fl_panel_nivel1);
+		
+		textField = new JTextField();
+		panel_nivel1.add(textField);
+		textField.setColumns(15);
+		
+		panel_nivel2 = new JPanel();
+		panel_Central.add(panel_nivel2);
+		panel_nivel2.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		btnNewButton_1 = new JButton("1");
+		panel_nivel2.add(btnNewButton_1);
+		
+		btnNewButton_2 = new JButton("2");
+		panel_nivel2.add(btnNewButton_2);
+		
+		btnNewButton_3 = new JButton("3");
+		panel_nivel2.add(btnNewButton_3);
+		
+		panel_nivel3 = new JPanel();
+		panel_Central.add(panel_nivel3);
+		panel_nivel3.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		btnNewButton_4 = new JButton("4");
+		panel_nivel3.add(btnNewButton_4);
+		
+		btnNewButton_5 = new JButton("5");
+		panel_nivel3.add(btnNewButton_5);
+		
+		btnNewButton_6 = new JButton("6");
+		panel_nivel3.add(btnNewButton_6);
+		
+		panel_nivel4 = new JPanel();
+		panel_Central.add(panel_nivel4);
+		panel_nivel4.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		btnNewButton_7 = new JButton("7");
+		panel_nivel4.add(btnNewButton_7);
+		
+		btnNewButton_8 = new JButton("8");
+		panel_nivel4.add(btnNewButton_8);
+		
+		btnNewButton_9 = new JButton("9");
+		panel_nivel4.add(btnNewButton_9);
+		
+		panel_nivel5 = new JPanel();
+		panel_Central.add(panel_nivel5);
+		panel_nivel5.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		btnNewButton_0 = new JButton("0");
+		panel_nivel5.add(btnNewButton_0);
+		
+		btnNewButton_Coma = new JButton(",");
+		btnNewButton_Coma.setFont(new Font("Tahoma", Font.BOLD, 11));
+		panel_nivel5.add(btnNewButton_Coma);
+		
+		panel_Izq = new JPanel();
+		frmCalcuBasic.getContentPane().add(panel_Izq, BorderLayout.EAST);
+		panel_Izq.setLayout(new BoxLayout(panel_Izq,BoxLayout.Y_AXIS));
+		
+		lblNewLabel = new JLabel(" ");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setIcon(new ImageIcon(CalculadoraBasica.class.getResource("/imag/fun2.png")));
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		panel_Izq.add(lblNewLabel);
+		
+		btnNewButton_AC = new JButton("AC");
+		panel_Izq.add(btnNewButton_AC);
+		
+		btnNewButton_Res = new JButton("=");
+		btnNewButton_Res.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		panel_Izq.add(btnNewButton_Res);
+		
+		frmCalcuBasic.pack();
 	}
 
 }
